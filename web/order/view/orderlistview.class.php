d54f82c2b576c1cd3c369329c96d43a1ebb9ab4b
Katie <ktkinkel@gmail.com>
2017-03-14 13:42:56 -0700

    fix

--- web/order/view/orderlistview.class.php
+++ web/order/view/orderlistview.class.php
@@ -390,7 +390,7 @@ class OrderListView extends AbstractListView {
 
                             <tr>
                                 <td colspan="6" style="text-align: right">
-                                    <span style="font-size: 0.7em; color: grey; float: left;">
+                                    <span style="font-size: 0.7em; color: grey; float: left; margin-top: 8px;">
 										<?php echo $statsMessage; ?>
                                     </span>
                                 </td>
